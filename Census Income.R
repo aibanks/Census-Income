fd7a52a91c9f03ae51db9593f3ff17def1af7638
Anthony Banks <anthonyianbanks@gmail.com>
2019-12-07 10:10:48 -0800

    Commit mid-modelling

---
+++ Census Income.R
@@ -0,0 +1,131 @@
+library(tidyverse)
+library(caret)
+library(ggplot2)
+
+file <- "adult-census-income.zip"
+
+dat <- read.csv(unzip(file))
+
+head(dat)
+
+dim(dat)
+
+sum(is.na(dat$income))
+
+test_index <- createDataPartition(y = dat$income, p = 0.1, list = FALSE)
+
+training_dat <- dat[-test_index,]
+testing_dat <- dat[test_index,]
+
+dim(training_dat)
+dim(testing_dat)
+
+#####################################
+##Data inspection and visualization##
+#####################################
+training_dat %>% group_by(income) %>% 
+  summarize(Count=n(), Percent = n()/(length(training_dat$income)))
+
+age_dense <- training_dat %>% ggplot(aes(age, fill = income)) + geom_density(stat ="count") 
+age_dense
+
+gender_hist <- training_dat %>% ggplot(aes(sex, fill = income)) + geom_bar(position = position_dodge())
+gender_hist
+
+education_hist <- training_dat %>% ggplot(aes(education.num, fill = income)) + geom_histogram(binwidth = 1, position = position_dodge()) #+ theme(axis.text.x = element_text(angle = 90, hjust = 1))
+education_hist
+
+race_hist <- training_dat %>% ggplot(aes(race, fill = income)) + geom_histogram(stat="count", position = position_dodge())
+race_hist
+
+workclass_hist <- training_dat %>% ggplot(aes(workclass, fill = income)) + geom_histogram(stat = "count", position = position_dodge()) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+workclass_hist
+
+#hours.per.week_hist <- training_dat %>% ggplot(aes(hours.per.week, fill = income)) +geom_histogram(binwidth = 10, position = position_dodge(), col="black")
+#hours.per.week_hist
+
+hours.per.week_freqpoly <- training_dat %>% ggplot(aes(hours.per.week, colour = income)) + geom_freqpoly(binwidth = 6) + scale_x_continuous(limits=c(0, 80))
+hours.per.week_freqpoly
+
+marital.status_hist <- training_dat %>% ggplot(aes(marital.status, fill = income)) + geom_histogram(stat = "count", position = position_dodge()) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+marital.status_hist
+
+native.country_hist <- training_dat %>% ggplot(aes(native.country, fill = income)) + geom_histogram(stat = "count", position = position_dodge()) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+native.country_hist
+
+native.county_table <- training_dat %>% group_by(native.country) %>%
+  summarize(sum(income == "<=50K"), sum(income == ">50K"))
+native.county_table
+
+native.county_table <- native.county_table %>% mutate("percent_>50K" = (`sum(income == ">50K")`)/(`sum(income == ">50K")` + `sum(income == "<=50K")`)*100)
+
+native.county_table <- native.county_table %>% arrange(desc(`percent_>50K`))
+
+head(native.county_table, 5)
+tail(native.county_table, 5)
+
+########################################
+####Data Cleanse########################
+########################################
+training_dat <- training_dat %>% select(-c(fnlwgt,relationship, education.num))
+
+#########################################
+#####Start Models########################
+#########################################
+
+#Model 1A: Linear Regression 
+training_dat_as.num <- training_dat %>% mutate(income = as.numeric(income == ">50K"))
+train_lm <- train(income ~., method = "lm", data = training_dat_as.num)
+income_hat_as.num <- predict(train_lm, testing_dat)
+income_hat <- ifelse(income_hat_as.num > 0.5, ">50K", "<=50K") %>% factor()
+
+confusionMatrix(data = income_hat, reference = testing_dat$income)
+model.results_linear.regression <- confusionMatrix(data = income_hat, reference = testing_dat$income)
+#accuracy = 0.8299, sensitivity = 0.9361, specificity = 0.4955
+
+
+#Model 2: KNN
+train_knn <- train(income ~., 
+                  method = "knn",
+                  data = training_dat)
+
+ggplot(train_knn, highlight = TRUE)
+
+#2.2 KNN with CV and expanded K values
+#control <- trainControl(method = "cv", number = 10, p = .9)
+
+train_knn_cv <- train(income ~.,
+                      method = "knn",
+                      data = training_dat,
+                      tuneGrid = data.frame(k = seq(9, 40, 5)))
+                      #trContol = control
+ggplot(train_knn_cv, highlight = TRUE)
+#taking a very very long time (>30 minutes)
+#attempt without cv;  started at 10:01
+#Model 3A: Regression Tree - standard (no adjusting tuning parameters)
+train_tree <- train(income ~ ., 
+                    method = "rpart",
+                    data = training_dat)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
