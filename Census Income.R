f4fbffa505fe0853d9164e1d940a98f9af6236ee
Anthony Banks <anthonyianbanks@gmail.com>
2019-12-07 11:24:20 -0800

    Additional commit, mid-modelling. 20191207 11:24

--- Census Income.R
+++ Census Income.R
@@ -1,6 +1,14 @@
 library(tidyverse)
 library(caret)
 library(ggplot2)
+library(rpart)				       
+
+library(rattle)					
+library(rpart.plot)			
+#library(RColorBrewer)				
+#library(party)					
+#library(partykit)
+
 
 file <- "adult-census-income.zip"
 
@@ -106,19 +114,52 @@ ggplot(train_knn_cv, highlight = TRUE)
 train_tree <- train(income ~ ., 
                     method = "rpart",
                     data = training_dat)
+plot(train_tree, margin = 0.1)
+text(train_tree, cex = 0.75)
+#started at 10:14; ended ~10:15
+
+#Model 3B: Regression Tree - adjusting complexity parameter (cp)
+train_tree_cp <- train(income ~., 
+                       method = "rpart",
+                       tuneGrid = data.frame(cp = seq(0, 0.05, len = 25)),
+                       data = training_dat)
+
+ggplot(train_tree_cp)
+#started at 10:22; ended ~10:24
+plot(train_tree_cp$finalModel)
+text(train_tree_cp$finalModel, cex = 0.5)
+
+prp(train_tree_cp$finalModel)
+fancyRpartPlot(train_tree_cp$finalModel)
+
+income_hat_tree_cp <- predict(train_tree_cp, testing_dat)
+confusionMatrix(income_hat_tree_cp, reference = testing_dat$income)
+model.results_regression.tree_cp <- confusionMatrix(income_hat_tree_cp, reference = testing_dat$income)
+
+#Accuracy= 0.8545, Sensitivity = 0.9434, Specificity = 0.5745
+#Increased all three, but the specificity is still very low.  Will try with forest instead of tree
 
 
+#Model 4A: Random Forest - Standard (without manually adjusting tuning parameters)
+train_random.forest <- train(income ~., 
+                             method = "Rborist",
+                             data = training_dat)
+#started at 10:52; ended 11:17
+plot(train_random.forest)
 
+income_hat_random.forest <- predict(train_random.forest, testing_dat)
+confusionMatrix(income_hat_random.forest, reference = testing_dat$income)
 
+#Accuracy = 0.759,  Sensitivity = 1.00, Specificity = 0.00
 
+#Model 5A:  Random Forest - Adjusting the minNode
+train_random.forest_minNode <- train(income ~ .,
+                                     method = "Rborist",
+                                     tuneGrid = data.frame(predFixed = 2, minNode = c(3, 50)),
+                                     data = training_dat)
 
-
-
-
-
-
-
-
+income_hat_random.forest_minNode <- predict(train_random.forest_minNode, testing_dat)
+confusionMatrix(income_hat_random.forest_minNode, reference = testing_dat$income)
 
 
 
