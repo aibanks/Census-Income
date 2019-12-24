d2e51482d53316b7566b5299214a7592d194e0ad
Anthony Banks <anthonyianbanks@gmail.com>
2019-12-24 10:28:00 -0800

    20191224 10:27.  Finalizing code for input into RMD.

--- Census Income.R
+++ Census Income.R
@@ -98,18 +98,18 @@ training_dat <- training_dat %>% select(-c(fnlwgt,relationship, education))
 #########################################
 
 
-#Model 0:  Predicting <=50K for each entry because that's most common in our dataset
+#Model 1:  Predicting <=50K for each entry because that's most common in our dataset
 training_dat %>% group_by(income) %>% summarize(n=n())
-
-
 income_pred <- rep("<=50K", length(testing_dat$income)) %>% factor()
-
 confusionMatrix(income_pred, reference = testing_dat$income)
 
-#Accuracy = 0.759, Sensitivity = 1.00, Specificity = 0.00
+results1 <- confusionMatrix(income_pred, reference = testing_dat$income)
+'Model 1' <- as.table(c(results1$byClass["F1"] , results1$overall["Accuracy"], 
+                                results1$byClass["Sensitivity"] , results1$byClass["Specificity"]))
+results_table <- rbind(`Model 1`)
+results_table
 
-
-#Model 1: Linear Regression 
+#Model 2: Linear Regression 
 training_dat_as.num <- training_dat %>% mutate(income = as.numeric(income == ">50K"))
 train_lm <- train(income ~., method = "lm", data = training_dat_as.num)
 income_hat_as.num <- predict(train_lm, testing_dat)
@@ -117,11 +117,18 @@ income_hat <- ifelse(income_hat_as.num > 0.5, ">50K", "<=50K") %>% factor()
 
 confusionMatrix(data = income_hat, reference = testing_dat$income)
 model.results_linear.regression <- confusionMatrix(data = income_hat, reference = testing_dat$income)
+
+results2 <- confusionMatrix(data = income_hat, reference = testing_dat$income)
+'Model 2' <- as.table(c(results2$byClass["F1"] , results2$overall["Accuracy"], 
+                                results2$byClass["Sensitivity"] , results2$byClass["Specificity"]))
+results_table <- rbind(results_table, `Model 2`)
+results_table
+
 #accuracy = 0.8299, sensitivity = 0.9361, specificity = 0.4955
 #increased to:  accuracy = 0.84, sensitivity = 0.943, specificity = 0.5159
 
 
-#Model 2: Generalized Linear Model (GLM)
+#Model 3: Generalized Linear Model (GLM)
 train_glm <- train(income ~.,
                    method = "glm",
                    data = training_dat)
@@ -130,31 +137,14 @@ income_hat_glm <- predict(train_glm, testing_dat)
 #started at 14:08 ended at 14:10
 
 confusionMatrix(income_hat_glm, reference = testing_dat$income)
-#Increased to Accuracy = 0.8554, Sensitivity = 0.9373, Specificity = 0.5975
 
+results3 <- confusionMatrix(income_hat_glm, reference = testing_dat$income)
+'Model 3' <- as.table(c(results3$byClass["F1"] , results3$overall["Accuracy"], 
+                                results3$byClass["Sensitivity"] , results3$byClass["Specificity"]))
+results_table <- rbind(results_table, `Model 3`)
+results_table
 
-#Model 2: KNN
-train_knn <- train(income ~., 
-                   method = "knn",
-                   data = training_dat)
-
-ggplot(train_knn, highlight = TRUE)
-
-#2.2 KNN with CV and expanded K values
-#control <- trainControl(method = "cv", number = 10, p = .9)
-
-train_knn_cv <- train(income ~.,
-                      method = "knn",
-                      data = training_dat,
-                      tuneGrid = data.frame(k = seq(9, 40, 5)))
-#trContol = control
-ggplot(train_knn_cv, highlight = TRUE)
-#taking a very very long time (>30 minutes)
-#attempt without cv;  started at 10:01
-
-
-
-#Model 3A: Regression Tree - standard (no adjusting tuning parameters)
+#Model 4A: Regression Tree - standard (no adjusting tuning parameters)
 train_tree <- train(income ~ ., 
                     method = "rpart",
                     data = training_dat)
@@ -163,11 +153,17 @@ text(train_tree, cex = 0.75)
 
 income_hat_tree <- predict(train_tree, testing_dat)
 confusionMatrix(income_hat_tree, reference = testing_dat$income)
-#Accuracy = 0.8397, Sensitivity = 0.9543, Specificity = 0.4790
 
-#started at 10:14; ended ~10:15
+results4a <- confusionMatrix(income_hat_tree, reference = testing_dat$income)
 
-#Model 3B: Regression Tree - adjusting complexity parameter (cp)
+
+'Model 4A' <- as.table(c(results4a$byClass["F1"] , results4a$overall["Accuracy"], 
+                               results4a$byClass["Sensitivity"] , results4a$byClass["Specificity"]))
+
+results_table <- rbind(results_table, `Model 4A`)
+results_table
+
+#Model 4B: Regression Tree - adjusting complexity parameter (cp)
 train_tree_cp <- train(income ~., 
                        method = "rpart",
                        tuneGrid = data.frame(cp = seq(0, 0.05, len = 25)),
@@ -183,172 +179,14 @@ fancyRpartPlot(train_tree_cp$finalModel)
 
 income_hat_tree_cp <- predict(train_tree_cp, testing_dat)
 confusionMatrix(income_hat_tree_cp, reference = testing_dat$income)
-model.results_regression.tree_cp <- confusionMatrix(income_hat_tree_cp, reference = testing_dat$income)
+results4b <- confusionMatrix(income_hat_tree_cp, reference = testing_dat$income)
 
-#Increased to Accuracy = 0.8618, Sensitivity = 0.9620, Specificity = 0.5465
 
+'Model 4B' <- as.table(c(results4b$byClass["F1"] , results4b$overall["Accuracy"], 
+                               results4b$byClass["Sensitivity"] , results4b$byClass["Specificity"]))
 
-#Model 3C: Regression Tree w. Cross Validation
-
-control <- trainControl(method = "repeatedcv", 
-                        repeats = 10)
-                      
-train_tree_cv <- train(income ~ .,
-                       data = training_dat,
-                       method = "rpart",
-                       tuneGrid = data.frame(cp = seq(0, 0.05, len = 25)),
-                       trControl = control)
-
-income_hat_tree_cv <- predict(train_tree_cv, testing_dat)
-
-confusionMatrix(income_hat_tree_cv, testing_dat$income)
-
-#Model 4A: Random Forest - Standard (without manually adjusting tuning parameters)
-train_random.forest <- train(income ~., 
-                             method = "rf",
-                             metric = "Accuracy",
-                             trControl = trainControl(),
-                             tuneGrid = NULL,
-                             data = training_dat)
-
-#started at 10:52; ended 11:17
-#started at 15:53;didn't end by 16:54 (manually stopped)
-plot(train_random.forest)
-
-income_hat_random.forest <- predict(train_random.forest, testing_dat)
-confusionMatrix(income_hat_random.forest, reference = testing_dat$income)
-
-#Accuracy = 0.759,  Sensitivity = 1.00, Specificity = 0.00
-
-#Model 4B:  Random Forest - Adjusting the minNode
-train_random.forest_minNode <- train(income ~ .,
-                                     method = "Rborist",
-                                     tuneGrid = data.frame(predFixed = 2, minNode = c(3, 50)),
-                                     data = training_dat)
-
-income_hat_random.forest_minNode <- predict(train_random.forest_minNode, testing_dat)
-confusionMatrix(income_hat_random.forest_minNode, reference = testing_dat$income)
-#started at 11:24; finished by 12:15 (left to get groceries)
-
-#Accuracy = 0.759,  Sensitivity = 1.00, Specificity = 0.00
-
-
-#Model 4C: Random Forest on smaller training set (30%)
-
-rf_train_index <- createDataPartition(training_dat$income, times = 1, p = 0.05, list = FALSE)
-rf_train_dat <- training_dat[rf_train_index,]
-rf_else_dat <- training_dat[-rf_train_index,]
-
-train_rf_lim.dat <- train(income ~ .,
-                          method = "Rborist",
-                          data = rf_train_dat)
-#started 17:54; stopped at 18:21
-
-
-#################################################
-###Testing Out Undersampling the Training set####
-#################################################
-
-training_dat_undersample <- downSample(training_dat, training_dat$income)
-
-mean(training_dat_undersample$income == "<=50K")
-
-training_dat_undersample <- training_dat_undersample %>% select(-c(Class))
-
-
-
-
-
-#Model 3A: Regression Tree - standard (no adjusting tuning parameters)
-train_tree <- train(income ~ ., 
-                    method = "rpart",
-                    data = training_dat_undersample)
-plot(train_tree, margin = 0.1)
-text(train_tree, cex = 0.75)
-
-income_hat_tree <- predict(train_tree, testing_dat)
-confusionMatrix(income_hat_tree, reference = testing_dat$income)
-#Accuracy = 0.8397, Sensitivity = 0.9543, Specificity = 0.4790
-#Accuracy = 0.7157, Sensitivity = 0.6667, Specificity = 0.8701, balanced Acc. = 0.7684
-
-
-
-#Model 3B: Regression Tree - adjusting complexity parameter (cp)
-train_tree_cp <- train(income ~., 
-                       method = "rpart",
-                       tuneGrid = data.frame(cp = seq(0, 0.05, len = 25)),
-                       data = training_dat_undersample)
-
-#started at 10:22; ended ~10:24
-
-income_hat_tree_cp <- predict(train_tree_cp, testing_dat)
-confusionMatrix(income_hat_tree_cp, reference = testing_dat$income)
-model.results_regression.tree_cp <- confusionMatrix(income_hat_tree_cp, reference = testing_dat$income)
-
-#Increased to Accuracy = 0.8618, Sensitivity = 0.9620, Specificity = 0.5465
-#Acc. = 0.79, Sensitivity = 0.7686, Specificity = 0.8573, Balanced Acc. = 0.8130
-
-
-##################################################################
-###Testing Out Undersampling the Training set and the Test set####
-##################################################################
-
-training_dat_undersample <- downSample(training_dat, training_dat$income)
-
-mean(training_dat_undersample$income == "<=50K")
-
-training_dat_undersample <- training_dat_undersample %>% select(-c(Class))
-
-
-testing_dat_undersample <- downSample(testing_dat, testing_dat$income)
-
-mean(testing_dat_undersample$income == "<=50K")
-
-testing_dat_undersample <- testing_dat_undersample %>% select(-c(Class))
-
-##################################
-
-#Model 3A: Regression Tree - standard (no adjusting tuning parameters)
-train_tree <- train(income ~ ., 
-                    method = "rpart",
-                    data = training_dat_undersample)
-
-
-income_hat_tree <- predict(train_tree, testing_dat_undersample)
-confusionMatrix(income_hat_tree, reference = testing_dat_undersample$income)
-#Original:  Accuracy = 0.8397, Sensitivity = 0.9543, Specificity = 0.4790
-#Undersampled Training Set:  Accuracy = 0.7157, Sensitivity = 0.6667, Specificity = 0.8701, balanced Acc. = 0.7684
-#Undersampled Test and Training Sets:  Accuracy = 0.7707, Sens. = 0.6713, Spec = 0.8701, Balanced Acc. = 0.7707
-
-#Model 5: AdaBoost Classification Trees
-
-train_glmboost <- train(income~.,
-                        method = "glmboost",
-                        data = training_dat)
-
-income_hat_glmboost <- predict(train_glmboost, testing_dat)
-confusionMatrix(income_hat_glmboost, reference = testing_dat$income)
-
-#started at 18:43  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+results_table <- rbind(results_table, `Model 4B`)
+results_table
 
 
 
