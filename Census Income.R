ea2ca80e172c8ac852af3eb9d24dba193388c3fd
Anthony Banks <anthonyianbanks@gmail.com>
2019-12-26 17:48:36 -0800

    Updated Code, RMD, and HTML.  Ready for EDX assignment upload.

--- Census Income.R
+++ Census Income.R
@@ -1,4 +1,4 @@
-#####Update the library sections into "IF REQUIRE"
+#####Install the required packages (if not already installed), and load them in.
 if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
 if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
 if(!require(ggplot2)) install.packages("data.table", repos = "http://cran.us.r-project.org")
@@ -21,82 +21,79 @@ library(mboost)
 library(plyr)
 library(knitr)
 
-install.packages("recipes")
 
-
+##Read in the file
 file <- "adult-census-income.zip"
-
 dat <- read.csv(unzip(file))
 
+##Check the first entries and the dimensions of the dataset
 head(dat)
-
 dim(dat)
 
+##Check for any missing values or NA values in the dataset
 sum(is.na(dat$income))
 
+##Set the seed for reproducibility, then partition the original dataset into a training dataset
+## with 90% of the original dataset and a testing dataset with 10% of the original dataset.
+set.seed(1993, sample.kind = "Rounding")
 test_index <- createDataPartition(y = dat$income, p = 0.1, list = FALSE)
-
 training_dat <- dat[-test_index,]
 testing_dat <- dat[test_index,]
 
+##Check the dimensions of the training and testing datasets
 dim(training_dat)
 dim(testing_dat)
 
 #####################################
 ##Data inspection and visualization##
 #####################################
+
+#Table showing the distribution of income entries among the training dataset
 training_dat %>% group_by(income) %>% c(Count=n(), Percent = n()/(length(training_dat$income))*100)
 
-
-
+#Graph showing the age distribution of the income categories
 age_dense <- training_dat %>% ggplot(aes(age, fill = income)) + geom_density(stat ="count") + ggtitle("Graph 1. Age Distribution")
 age_dense
 
+#Histogram of gender of the income categories
 gender_hist <- training_dat %>% ggplot(aes(sex, fill = income)) + geom_bar(position = position_dodge()) + ggtitle("Graph 2. Gender Distribution")
 gender_hist
 
+#Histogram of the years of education of the income categories
 education_hist <- training_dat %>% ggplot(aes(education.num, fill = income)) + geom_histogram(binwidth = 1, position = position_dodge()) + ggtitle("Graph 3. Education Distribution")
 education_hist
 
+#Histogram of the races of the income categories
 race_hist <- training_dat %>% ggplot(aes(race, fill = income)) + geom_histogram(stat="count", position = position_dodge()) + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle("Graph 4. Race Distribution")
 race_hist
 
+#Table of the races of the income categories
 race_table <- training_dat %>% group_by(race) %>% 
   dplyr::summarise('Count <=50K' = sum(income == "<=50K"), 'Count >50K' = sum(income == ">50K"))
 race_table <- race_table %>% mutate('Percent >50K' = (`Count >50K`) / ((`Count <=50K`) + (`Count >50K`)) *100)
 race_table
 
-race.edu_graph <- training_dat %>% ggplot(aes(race, education.num, color = income)) + geom_boxplot()
-race.edu_graph
-
+#Histogram of the workclasses of the income categories
 workclass_hist <- training_dat %>% ggplot(aes(workclass, fill = income)) + geom_histogram(stat = "count", position = position_dodge()) + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle("Graph 5. Workclass Distribution")
 workclass_hist
 
-#hours.per.week_hist <- training_dat %>% ggplot(aes(hours.per.week, fill = income)) +geom_histogram(binwidth = 10, position = position_dodge(), col="black")
-#hours.per.week_hist
-
+#Graph of the number of hours worked per week of the income categories
 hours.per.week_freqpoly <- training_dat %>% ggplot(aes(hours.per.week, colour = income)) + geom_freqpoly(binwidth = 6) + scale_x_continuous(limits=c(0, 80)) + ggtitle("Graph 6. Hours Worked Per Week Distribution")
 hours.per.week_freqpoly
 
+#Histogram of the marital status of the income categories
 marital.status_hist <- training_dat %>% ggplot(aes(marital.status, fill = income)) + geom_histogram(stat = "count", position = position_dodge()) + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle("Graph 7. Marital Status Distribution")
 marital.status_hist
-
-native.country_hist <- training_dat %>% ggplot(aes(native.country, fill = income)) + geom_histogram(stat = "count", position = position_dodge()) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
-native.country_hist
-
-native.county_table <- training_dat %>% group_by(native.country) %>%
-  summarize(sum(income == "<=50K"), sum(income == ">50K"))
-native.county_table
 
 ########################################
 ####Data Cleanse########################
 ########################################
+#Removing the columns that I do not want to use in the model
 training_dat <- training_dat %>% select(-c(fnlwgt,relationship, education))
 
 #########################################
 #####Start Models########################
 #########################################
-
 
 #Model 1:  Predicting <=50K for each entry because that's most common in our dataset
 training_dat %>% group_by(income) %>% summarize(n=n())
@@ -124,18 +121,12 @@ results2 <- confusionMatrix(data = income_hat, reference = testing_dat$income)
 results_table <- rbind(results_table, `Model 2`)
 results_table
 
-#accuracy = 0.8299, sensitivity = 0.9361, specificity = 0.4955
-#increased to:  accuracy = 0.84, sensitivity = 0.943, specificity = 0.5159
-
 
 #Model 3: Generalized Linear Model (GLM)
 train_glm <- train(income ~.,
                    method = "glm",
                    data = training_dat)
-
 income_hat_glm <- predict(train_glm, testing_dat)
-#started at 14:08 ended at 14:10
-
 confusionMatrix(income_hat_glm, reference = testing_dat$income)
 
 results3 <- confusionMatrix(income_hat_glm, reference = testing_dat$income)
@@ -169,12 +160,8 @@ train_tree_cp <- train(income ~.,
                        tuneGrid = data.frame(cp = seq(0, 0.05, len = 25)),
                        data = training_dat)
 
-ggplot(train_tree_cp, highlight = TRUE)
-plot(train_tree_cp$finalModel)
-text(train_tree_cp$finalModel, cex = 0.5)
-
-prp(train_tree_cp$finalModel)
-fancyRpartPlot(train_tree_cp$finalModel)
+ggplot(train_tree_cp, highlight = TRUE) + ggtitle("Graph 8. Estimated Accuracy vs Complexity Parameter in Model 4B")
+prp(train_tree_cp$finalModel, main = "Figure 1. Decision Tree of Model 4B")
 
 income_hat_tree_cp <- predict(train_tree_cp, testing_dat)
 confusionMatrix(income_hat_tree_cp, reference = testing_dat$income)
@@ -186,6 +173,3 @@ results4b <- confusionMatrix(income_hat_tree_cp, reference = testing_dat$income)
 
 results_table <- rbind(results_table, `Model 4B`)
 results_table
-
-
-
